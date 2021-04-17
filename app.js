e1b9f0266067a758e3d122e5ca48ed837e07721e
jjnorris31 <jjnorris31@gmail.com>
2021-04-17 18:34:19 -0500

    feat: added dotenv support

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,9 +19,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.listen(3000, () => {
-    console.log('listen at 3000!');
-})
 
 module.exports = app;
