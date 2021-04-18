baeb1fe758838d7068f56c51490ccdf04e21abfc
jjnorris31 <jjnorris31@gmail.com>
2021-04-17 20:32:27 -0500

    feat: added typescript support

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+import {User} from './controllers/User'
+
+const myUser = new User('JuanJo', 'Basilio', 'jjnorris31@gmail.com', '1234');
+console.log({myUser});
+
+myUser.name = 'José';
+console.log({myUser});
+
 
 var admin = require("firebase-admin");
-var serviceAccount = require("./graduatify-firebase-admin.json");
+var serviceAccount = require("../graduatify-firebase-admin.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -13,10 +21,8 @@ admin.initializeApp({
 
 console.log({admin});
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('../routes');
+var usersRouter = require('../routes/users');
 
 var app = express();
 
@@ -29,5 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(process.env.PORT);
+})
 
 module.exports = app;
